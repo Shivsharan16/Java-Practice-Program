4089dff65a82f32d9876ee54f590e23704c6e120
Shivsharan <shivsharangiram16@gmail.com>
2024-07-29 15:04:44 +0530

    Prints Sum of digits

---
+++ SumOfDigits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num, digit, sum=0;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        num=sc.nextInt();
+        while(num>0){
+            digit=num%10;
+            sum=sum+digit;
+            num=num/10;
+        }
+        System.out.println("sum of digits: " +sum);
+    }
+}
